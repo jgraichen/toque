d18efca25620bf5ff22e467abd85e0d989c43d96
Jan Graichen <jg@altimos.de>
2013-04-07 21:23:29 +0200

    Fix toque spec.

--- spec/spec_toque.rb
+++ spec/spec_toque.rb
@@ -40,7 +40,7 @@ describe Toque do
     @configuration.awesome!
 
     # should have installed chef version
-    expect(@configuration.capture('/opt/chef/bin/chef-solo -v')).to be == 'Chef: 10.24.0'
+    expect(@configuration.capture('/opt/chef/bin/chef-solo -v').strip).to be == 'Chef: 10.24.0'
 
     # should have executed cookbook and created an awesome file
     file_exists = @vagrant.primary_vm.channel.sudo 'ls /tmp/toque/awesome'
