88d1d6f6ac0f985c4643db341730c70ae8d335c9
Jan Graichen <jg@altimos.de>
2013-04-07 13:38:47 +0200

    Set version.

--- lib/toque/version.rb
+++ lib/toque/version.rb
@@ -1,3 +1,12 @@
 module Toque
-  VERSION = "0.0.1"
+  module VERSION
+    MAJOR = 1
+    MINOR = 0
+    PATCH = 0
+    STAGE = :dev
+
+    STRING = [MAJOR, MINOR, PATCH, STAGE].reject(&:nil?).join('.')
+
+    def self.to_s; STRING end
+  end
 end
